e277ba536914e9916ffc549b999fcfe0b306015d
ronitsavaliya03 <ronitsavaliya3303@gmail.com>
2024-09-07 20:43:17 +0530

    add comment, like, playlist, and tweet models

---
+++ src/models/comment.model.js
@@ -0,0 +1,20 @@
+import mongoose, { Schema } from "mongoose";
+
+const commentSchema= new Schema({
+    content:{
+        type: String,
+        require: true
+    },
+    video: {
+        type: Schema.Types.ObjectId,
+        ref: "Video"
+    },
+    owner: {
+        type: Schema.Types.ObjectId,
+        ref: "User"
+    }
+}, {timestamps: true})
+
+commentSchema.plugin(mongooseAggregatePaginate)
+
+export const Comment =mongoose.model("Comment", commentSchema)
---
+++ src/models/playlist.model.js
@@ -0,0 +1,23 @@
+import mongoose, { Schema } from "mongoose";
+
+const playlistSchema= new Schema({
+    name: {
+        type: String,
+        require: true
+    },
+    description: {
+        type: String,
+        require: true
+    },
+    video: [{
+        type: Schema.Types.ObjectId,
+        ref: "Video"
+    }],
+    owner: {
+        type: Schema.Types.ObjectId,
+        ref: "User"
+    }
+
+}, {timestamps: true})
+
+export const Playlist= mongoose.model("Playlist", playlistSchema)
